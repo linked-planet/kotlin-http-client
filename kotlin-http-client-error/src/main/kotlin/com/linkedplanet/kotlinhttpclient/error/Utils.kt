45641f47812ad2953a41e74348e236029e8f6834
Philipp Karlsson <philipp.karlsson@link-time.com>
2022-03-24 13:37:51 +0100

    fixing recursiveRestCall to suspend

--- kotlin-http-client-error/src/main/kotlin/com/linkedplanet/kotlinhttpclient/error/Utils.kt
+++ kotlin-http-client-error/src/main/kotlin/com/linkedplanet/kotlinhttpclient/error/Utils.kt
@@ -9,7 +9,7 @@ import com.google.gson.GsonBuilder
 
 val GSON: Gson = GsonBuilder().create()
 
-fun <T> recursiveRestCall(start: Int = 0, max: Int? = null, call: (Int, Int) -> Either<DomainError, List<T>>): Either<DomainError, List<T>> {
+suspend fun <T> recursiveRestCall(start: Int = 0, max: Int? = null, call: suspend (Int, Int) -> Either<DomainError, List<T>>): Either<DomainError, List<T>> {
     var index = start
     val maxResults = 1
     val elements = mutableListOf<T>()
